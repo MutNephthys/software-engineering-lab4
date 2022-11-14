e3ab39b52a7aff090453d47f2547fd8dbe28d7c4
MutNephthys <201220055@smail.nju.edu.cn>
2022-11-14 15:08:48 +0800

    compare

--- lab4-code/compare.sh
+++ lab4-code/compare.sh
@@ -4,13 +4,15 @@ touch equal.csv
 touch inequal.csv
 echo "file1,file2" > equal.csv
 echo "file1,file2" > inequal.csv
+declare -A dict_equal
+declare -A dict_inequal
 for tmp_dir in $(ls ./workdir); do
     dir="workdir/"$tmp_dir
     #dir=/workdir/input0
-    declare -A dict
+    declare -A dict_nonrepeatable
     for file1 in $dir/*.txt; do
         for file2 in $dir/*.txt; do
-            if [ $file1 != $file2 ] && [ ! -v dict[$file2] ]
+            if [ $file1 != $file2 ] && [ ! -v dict_nonrepeatable[$file2] ]
             then 
                 tmp1=${file1%input*}
                 tmp11=${tmp1##*/}
@@ -19,12 +21,33 @@ for tmp_dir in $(ls ./workdir); do
                 tmp22=${tmp2##*/}
                 tmp222="input/dir/"$tmp22".cpp"
                 if diff $file1 $file2 > /dev/null; then
-                    echo $tmp111","$tmp222 >> equal.csv
+                    if [ ! -v dict_equal[$tmp111","$tmp222] ]
+                    then
+                        dict_equal[$tmp111","$tmp222]=1
+                        #echo $tmp111","$tmp222"    1"
+                    fi
                 else
-                    echo $tmp111","$tmp222 >> inequal.csv
+                    if [ ! -v dict_inequal[$tmp111","$tmp222] ]
+                    then
+                        echo $tmp111","$tmp222 >> inequal.csv
+                        dict_inequal[$tmp111","$tmp222]=1
+                    fi
+                    if [ -v dict_equal[$tmp111","$tmp222] ]
+                    then 
+                        dict_equal[$tmp111","$tmp222]=0
+                        #echo $tmp111","$tmp222"    0"
+
+                    fi
                 fi
             fi
         done
-        dict[$file1]=1
+        dict_nonrepeatable[$file1]=1
     done
+done
+for key in $(echo ${!dict_equal[*]})
+do
+    if [ dict_equal[$key]==1 ]
+    then
+        echo $key >> equal.csv
+    fi
 done
